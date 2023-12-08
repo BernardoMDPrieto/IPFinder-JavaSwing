dddca858a1c4311b37d5c88090d9f9ecb17cb32c
Bernardo Prieto <123491333+BernardoMDPrieto@users.noreply.github.com>
2023-12-08 11:23:34 -0300

    Add files via upload

---
+++ IPFinder.java
@@ -0,0 +1,55 @@
+package JavaSwingIPFinder;
+
+import javax.swing.*;  
+import java.net.*;
+import java.awt.event.*;
+
+
+public class IPFinder extends JFrame implements ActionListener{
+	String url = "";
+	JLabel l;
+	JTextField tf;
+	JButton b;
+		
+	IPFinder(){
+		super("IP Finder Tool - JavaTpoint");
+		l = new JLabel("Enter URL");
+		l.setBounds(50,70,150,20);
+		tf = new JTextField();
+		tf.setBounds(50,100,200,20);
+	
+		b = new JButton("Find IP");
+		b.setBounds(50,150,80,30);
+		b.addActionListener(this);
+		add(l);
+		add(tf);
+		add(b);
+		
+		
+		setSize(300,300);
+		setLayout(null);
+		setVisible(true);
+	}
+
+	
+	public void actionPerformed(ActionEvent e) {
+		String url = tf.getText();
+		
+		try {
+			InetAddress ia=InetAddress.getByName(url);  
+			String ip = ia.getHostAddress();	
+			JOptionPane.showMessageDialog(this,ip);
+		} catch (UnknownHostException e1) {
+			JOptionPane.showMessageDialog(this,e1.toString());
+		}
+		
+		
+	}
+		
+	
+	
+	
+	
+	
+	
+}
